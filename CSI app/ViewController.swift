8be9d6395dbe52c7112a19216f6b873bb5c12db1
Dave Smith <Dave@daves-macbook-pro.local>
2016-02-08 17:09:31 -0500

    Initial Commit

---
+++ CSI app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSI app
+//
+//  Created by Dave Smith on 2/8/16.
+//  Copyright © 2016 David Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
